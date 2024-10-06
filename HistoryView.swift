37673360c86f5b9226f0de0b926fed1e29a6b88f
Mateo Andino <79855501+MateoNitro550@users.noreply.github.com>
2024-10-06 07:15:51 -0400

    Update HistoryView.swift

--- HistoryView.swift
+++ HistoryView.swift
@@ -1,9 +1,82 @@
 import SwiftUI
 
 struct HistoryView: View {
+    @State private var navigateToResultView = false
+    @State private var navigateToHistoryView = false
+    @State private var navigateToProfileView = false
+    @State private var navigateToNoteView = false
+    @State private var navigateToSettingsView = false
+    @State private var navigateToHomeView = false
     var body: some View {
-        VStack {
-            Text("HISTORY VIEW")
-        }
+        NavigationView {
+            ZStack {
+                VStack(spacing: 0) {
+                    // Title Section
+                    Button(action: {
+                        navigateToHistoryView = true
+                    }) {
+                        VStack {
+                            
+                        }
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Menu {
+                            
+                            Button(action: {
+                                navigateToHomeView = true
+                            }) {
+                                HStack{
+                                    Image(systemName: "house.circle")
+                                    Text("Home")
+                                }
+                            }
+                            
+                            Button(action: {
+                                navigateToProfileView = true
+                            }) {
+                                HStack{
+                                    Image(systemName: "person")
+                                    Text("Account")
+                                }
+                            }
+                            
+                            Button(action: {
+                                navigateToNoteView = true
+                            }) {
+                                HStack{
+                                    Image(systemName: "note.text")
+                                    Text("Notebook")
+                                }
+                            }
+                            Button(action: {
+                                navigateToSettingsView = true
+                            }) {
+                                HStack{
+                                    Image(systemName: "gear")
+                                    Text("Settings")
+                                }
+                            }
+                        } label: {
+                            HStack {
+                                Text("History")
+                                    .font(.title)
+                                    .foregroundColor(Color(hex: "#871BAC"))
+                                //                            Image("drop-down")
+                                //                                .resizable()
+                                //                                .frame(width: 20, height: 20)
+                                //                                .padding(.leading, 5)
+                                
+                            }
+                            Spacer()
+                            Divider().frame(width:500)
+                        }
+                        .padding()
+                        
+                    }
+                }
+            }
+        } // Toolbar Ended
     }
 }
